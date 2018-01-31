61b7632a23b984bdca8a541a69e54a308e83a946
Tim Paine <t.paine154@gmail.com>
2018-01-31 10:39:03 -0500

    updating requirements, adding rudimentary type detection #2

--- perspective/psp.py
+++ perspective/psp.py
@@ -1,5 +1,22 @@
 def psp(data):
     from IPython.display import display
     bundle = {}
-    bundle['application/psp'] = data
+    bundle['application/psp'] = _type_detect(data)
     display(bundle, raw=True)
+
+
+def _type_detect(data):
+    try:
+        import pandas as pd
+        if isinstance(data, pd.DataFrame):
+            return data.reset_index().to_json(orient='records')
+    except ImportError:
+        pass
+
+    try:
+        import lantern as l
+        if isinstance(data, l.LanternLive):
+            return data.path()
+    except ImportError:
+        pass
+    return data
