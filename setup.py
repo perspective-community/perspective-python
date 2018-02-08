84ded1c8b61ff0ddcb5b7285ca728723f2cf6cb9
Tim Paine <t.paine154@gmail.com>
2018-02-08 15:40:41 -0500

    .

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='perspective-python',
-    version='0.0.3,',
+    version='v0.0.3,',
     description='Analytics library',
     long_description=long_description,
     url='https://github.com/timkpaine/perspective-python',
