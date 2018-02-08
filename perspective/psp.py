449cd27fbbca70ce58a36d595f8907b951a10135
Tim Paine <t.paine154@gmail.com>
2018-02-08 14:56:11 -0500

    remove html encoding

--- perspective/psp.py
+++ perspective/psp.py
@@ -1,12 +1,4 @@
 from enum import Enum
-try:
-    from html import unescape  # python 3.4+
-except ImportError:
-    try:
-        from html.parser import HTMLParser  # python 3.x (<3.4)
-    except ImportError:
-        from HTMLParser import HTMLParser  # python 2.x
-    unescape = HTMLParser().unescape
 
 
 def psp(data, view='hypergrid', columns=None, rowpivots=None, columnpivots=None, aggregates=None, settings=False):
