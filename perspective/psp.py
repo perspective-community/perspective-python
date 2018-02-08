a49826efed275689c370a78db161de58c56ea09d
Tim Paine <t.paine154@gmail.com>
2018-02-08 14:53:57 -0500

    view passed through to ui

--- perspective/psp.py
+++ perspective/psp.py
@@ -1,14 +1,42 @@
-import ujson
+from enum import Enum
+try:
+    from html import unescape  # python 3.4+
+except ImportError:
+    try:
+        from html.parser import HTMLParser  # python 3.x (<3.4)
+    except ImportError:
+        from HTMLParser import HTMLParser  # python 2.x
+    unescape = HTMLParser().unescape
 
 
-def psp(data, layout=None):
+def psp(data, view='hypergrid', columns=None, rowpivots=None, columnpivots=None, aggregates=None, settings=False):
     from IPython.display import display
     bundle = {}
     bundle['application/psp'] = {
         'data': _type_detect(data),
-        'layout': ujson.dumps(layout or [])
+        'layout': _layout(view, columns, rowpivots, columnpivots, aggregates, settings)
     }
     display(bundle, raw=True)
+
+
+def _layout(view='hypergrid', columns=None, rowpivots=None, columnpivots=None, aggregates=None, settings=False):
+    import ujson
+    '''
+        view="hypergrid"  # grid
+        view="horizontal"  # horizontal
+        view="vertical"  # vertical
+        view="line"  # line
+        view="scatter"  # scatter
+    '''
+    ret = {}
+
+    ret['view'] = view.value if isinstance(view, View) else view
+
+    ret['columns'] = columns or ''
+    ret['row-pivots'] = rowpivots or ''
+    ret['column-pivots'] = columnpivots or ''
+    ret['aggregates'] = aggregates or ''
+    return ujson.dumps(ret)
 
 
 def _type_detect(data):
@@ -26,3 +54,32 @@ def _type_detect(data):
     except ImportError:
         pass
     return data
+
+
+class Aggregates(Enum):
+    ANY = 'any'
+    AVG = 'avg'
+    COUNT = 'count'
+    DISTINCT_COUNT = 'distinct_count'
+    DOMINANT = 'dominant'
+    FIRST = 'first'
+    LAST = 'last'
+    HIGH = 'high'
+    LOW = 'low'
+    MEAN = 'mean'
+    MEAN_BY_COUNT = 'mean_by_count'
+    MEDIAN = 'median'
+    PCT_SUM_PARENT = 'pct_sum_parent'
+    PCT_SUM_GRAND_TOTAL = 'pct_sum_grand_total'
+    SUM = 'sum'
+    SUM_ABS = 'sum_abs'
+    SUM_NOT_NULL = 'sum_not_null'
+    UNIQUE = 'unique'
+
+
+class View(Enum):
+    HYPERGRID = 'hypergrid'
+    VERTICAL = 'vertical'
+    HORIZONTAL = 'horizontal'
+    LINE = 'line'
+    SCATTER = 'scatter'
