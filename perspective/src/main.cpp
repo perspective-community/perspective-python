882b8a895bd4fc811d9b6d1309e1163a632491db
Tim Paine <t.paine154@gmail.com>
2018-07-27 12:35:01 -0400

    building .a not .so, add main back with nothing, readd DPSP_ENABLE_PYTHON for new bindings

---
+++ perspective/src/main.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2017, the Perspective Authors.
+ *
+ * This file is part of the Perspective library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+
+#include <perspective/base.h>
+#include <perspective/gnode.h>
+#include <perspective/table.h>
+#include <perspective/pool.h>
+#include <perspective/context_zero.h>
+#include <perspective/context_one.h>
+#include <perspective/context_two.h>
+#include <random>
+#include <cmath>
+#include <sstream>
+#include <perspective/sym_table.h>
+
+using namespace perspective;
+
+
+/**
+ * Main
+ */
+int
+main(int argc, char** argv)
+{
+    std::cout << "Perspective initialized successfully." << std::endl;
+}
