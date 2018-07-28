4db44a89cf54809e91001ce9317478792eb55a41
Tim Paine <t.paine154@gmail.com>
2018-07-28 19:20:19 -0400

    adding more inits and methods for t_schema

--- perspective/include/perspective/binding.hpp
+++ perspective/include/perspective/binding.hpp
@@ -15,13 +15,39 @@ BOOST_PYTHON_MODULE(libbinding)
 {
     def("test", test);
 
-    class_<perspective::t_schema>("t_schema", init<perspective::t_svec, perspective::t_dtypevec>());
+    class_<perspective::t_schema>("t_schema",
+        init<perspective::t_svec, perspective::t_dtypevec>())
+        .def(init<>())
+        .def(init<perspective::t_schema_recipe>())
+        .def("get_num_columns", &perspective::t_schema::get_num_columns)
+        .def("size", &perspective::t_schema::size)
+        .def("get_colidx", &perspective::t_schema::get_colidx)
+        .def("get_dtype", &perspective::t_schema::get_dtype)
+        .def("is_pkey", &perspective::t_schema::is_pkey)
+        .def("add_column", &perspective::t_schema::add_column)
+        .def("get_recipe", &perspective::t_schema::get_recipe)
+        .def("has_column", &perspective::t_schema::has_column)
+        .def("get_table_context", &perspective::t_schema::get_table_context)
+
+        // when returning const, need return_value_policy<copy_const_reference>
+        .def("columns", &perspective::t_schema::columns, return_value_policy<copy_const_reference>())
+        // .def("types", &perspective::t_schema::types, return_value_policy<copy_const_reference>())
+    ;
+
 
     // need boost:noncopyable for PSP_NON_COPYABLE
     class_<perspective::t_table, boost::noncopyable>("t_table", init<perspective::t_schema>())
 
+        .def("init", &perspective::t_table::init)
+        .def("clear", &perspective::t_table::clear)
+        .def("reset", &perspective::t_table::reset)
         .def("size", &perspective::t_table::size)
+        .def("reserve", &perspective::t_table::reserve)
+        .def("extend", &perspective::t_table::extend)
+        .def("set_size", &perspective::t_table::set_size)
+
         .def("num_columns", &perspective::t_table::num_columns)
+        .def("get_capacity", &perspective::t_table::get_capacity)
 
         // when returning const, need return_value_policy<copy_const_reference>
         .def("name", &perspective::t_table::name, return_value_policy<copy_const_reference>())
@@ -29,11 +55,12 @@ BOOST_PYTHON_MODULE(libbinding)
 
         // when multiple overloading methods, need to static_cast to specify
         .def("num_rows", static_cast<perspective::t_uindex (perspective::t_table::*)() const> (&perspective::t_table::num_rows))
-        .def("num_rows", static_cast<perspective::t_uindex (perspective::t_table::*)(const perspective::t_mask&) const> (&perspective::t_table::num_rows));
-
-
-        // .def("pprint", &perspective::t_table::pprint);
-        // .def("get_capacity", &perspective::t_table::get_capacity)
-
+        .def("num_rows", static_cast<perspective::t_uindex (perspective::t_table::*)(const perspective::t_mask&) const> (&perspective::t_table::num_rows))
+        
+        .def("pprint", static_cast<void (perspective::t_table::*)() const>(&perspective::t_table::pprint))
+        .def("pprint", static_cast<void (perspective::t_table::*)(perspective::t_uindex, std::ostream*) const>(&perspective::t_table::pprint))
+        .def("pprint", static_cast<void (perspective::t_table::*)(const perspective::t_str&) const>(&perspective::t_table::pprint))
+        .def("pprint", static_cast<void (perspective::t_table::*)(const perspective::t_uidxvec&) const>(&perspective::t_table::pprint))
+    ;
 }
 #endif
