e52d652f807e39cca76d3616862f3731a0a31cc3
Tim Paine <t.paine154@gmail.com>
2018-08-27 20:08:33 -0400

    working on streaming

--- perspective/widget.py
+++ perspective/widget.py
@@ -26,40 +26,44 @@ class PerspectiveWidget(Widget):
     schema = Dict(default_value={}).tag(sync=True)
     view = Unicode('hypergrid').tag(sync=True)
     columns = List(default_value=[]).tag(sync=True)
-    rowpivots = List(trait=Unicode(), default_value=[]).tag(sync=True)
+    rowpivots = List(trait=Unicode(), default_value=[]).tag(sync=True, o=True)
     columnpivots = List(trait=Unicode(), default_value=[]).tag(sync=True)
-    aggregates = Dict(trait=Dict(trait=Unicode()), default_value={}).tag(sync=True)
+    aggregates = Dict(trait=Unicode(), default_value={}).tag(sync=True)
     sort = List(default_value=[]).tag(sync=True)
 
     settings = Bool(True).tag(sync=True)
     dark = Bool(False).tag(sync=True)
 
     # FIXME
-    helper_config = Unicode('{}').tag(sync=True)
+    helper_config = Dict(default_value={}).tag(sync=True)
 
     def _get_data(self):
         return self._dat_orig
 
     def _set_data(self, value):
         typ, dat_orig, dat = type_detect(value)
-
         if isinstance(dat, str):
             # unconvertable, must be http/ws/sio/comm
-            dat = {}
-            self.set_trait('datasrc', dat)
+            data_src = dat
             self._dat_orig = dat_orig
+            dat = []
+            dat_orig = ''
         else:
+            data_src = ''
             self._dat_orig = dat_orig
-            self.set_trait('datasrc', 'static')
+            self.datasrc = 'static'
 
         if len(dat_orig) and typ:
             s = validate_schema(dat_orig, typ)
-            self.set_trait('schema', s)
-            self.set_trait('columns', list(map(lambda x: str(x), s.keys())))
+            self.schema = s
+            self.columns = list(map(lambda x: str(x), s.keys()))
         else:
-            self.set_trait('schema', {})
+            self.schema = {}
 
-        self.set_trait('_data', dat)
+        if data_src:
+            self.datasrc = data_src
+
+        self._data = dat
 
     # FIXME
     data = property(_get_data, _set_data)
@@ -112,7 +116,7 @@ class PerspectiveWidget(Widget):
         conf = config(proposal.value, self._dat_orig)
         return conf
 
-    def __init__(self, data, view='hypergrid', schema=None, columns=None, rowpivots=None, columnpivots=None, aggregates=None, sort=None, settings=True, dark=False, helper_config='', **kwargs):
+    def __init__(self, data, view='hypergrid', schema=None, columns=None, rowpivots=None, columnpivots=None, aggregates=None, sort=None, settings=True, dark=False, helper_config=None, **kwargs):
         super(PerspectiveWidget, self).__init__(**kwargs)
         self.view = view
         self.schema = schema or {}
@@ -123,5 +127,7 @@ class PerspectiveWidget(Widget):
         self.sort = sort or []
         self.settings = settings
         self.dark = dark
-        self.helper_config = helper_config
+        self.helper_config = helper_config or {}
+
+        # do last
         self.data = data
