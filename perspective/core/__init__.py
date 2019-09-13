72a0083250e422daab7ccde5c491f3d8caafac60
Tim Paine <t.paine154@gmail.com>
2019-09-13 11:51:47 -0400

    moved

--- perspective/core/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._version import __version__  # noqa: F401
-from .base import PerspectiveBaseMixin  # noqa: F401
-from .psp import psp  # noqa: F401
-from .view import View  # noqa: F401
-from .aggregate import Aggregate  # noqa: F401
-from .exception import PSPException  # noqa: F401
-from .widget import PerspectiveWidget  # noqa: F401
-from .web import PerspectiveHTTPMixin  # noqa: F401
--- perspective/core/exception.py
+++
@@ -1,2 +0,0 @@
-class PSPException(Exception):
-    pass
